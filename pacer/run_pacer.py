16bd0e8e3ea06fe30280cf266ebd7db874012633
Tyler Tisinger <tt549@cornell.edu>
2026-04-23 22:45:13 -0400

    test v12

--- pacer/run_pacer.py
+++ pacer/run_pacer.py
@@ -6,7 +6,7 @@ from pacer_pattern import GreenPacer, ConstantPacerWithPacer, DynamicPacer
 # Create the pacer object
 # pacer = GreenPacer()
 # pacer = ConstantPacerWithPacer()
-pacer = DynamicPacer()
+pacer = DynamicPacer(pace=[8,6,4,2])
 
 print("Starting dynamic pacer…  Press CTRL+C to stop.")
 
